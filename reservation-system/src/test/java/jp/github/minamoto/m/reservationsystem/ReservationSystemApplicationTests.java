bc094cb73b070290d71052565b085f53c54d2bdf
minamoto <mimoma987@gmail.com>
2026-01-24 00:03:08 +0900

    Initial commit: Spring Boot project setup

---
+++ reservation-system/src/test/java/jp/github/minamoto/m/reservationsystem/ReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.github.minamoto.m.reservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
